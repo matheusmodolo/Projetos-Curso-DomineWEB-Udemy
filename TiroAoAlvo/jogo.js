e1a7f0d7b1ad01de95b145713fbe9aebc22d7480
Matheus Módolo <nmmatheus@hotmail.com>
2023-07-25 17:45:49 -0300

    posicoes aleatorias

--- TiroAoAlvo/jogo.js
+++ TiroAoAlvo/jogo.js
@@ -1,12 +1,30 @@
 
-var altura = 0
-var largura = 0
+var largura = 0;
+var altura = 0;
 
 function ajustaTamanhoPalcoJogo() {
-	altura = window.innerHeight
-	largura = window.innerWidth
+	largura = window.innerWidth;
+	altura = window.innerHeight;
 
-	console.log(altura, largura)
+	console.log(largura, altura);
 }
 
-ajustaTamanhoPalcoJogo()
+ajustaTamanhoPalcoJogo();
+
+function posicaoRandomica() {
+	var posicaoX = Math.floor(Math.random() * largura) - 50;
+	posicaoX = posicaoX < 0 ? 0 : posicaoX;
+
+	var posicaoY = Math.floor(Math.random() * altura) - 50;
+	posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+	var alvo = document.createElement("img");
+	alvo.src = "imagens/alvo.png";
+	alvo.className = "alvo1";
+	alvo.style.left = posicaoX + "px";
+	alvo.style.top = posicaoY + "px";
+	alvo.style.position = "absolute";
+
+	document.body.appendChild(alvo);
+}
+
