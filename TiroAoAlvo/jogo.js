32ef1dffa1594bb9c076b7c17d87ea31f17f6cf6
Matheus Módolo <nmmatheus@hotmail.com>
2023-07-25 21:51:32 -0300

    Tornando o código mais dinâmico

--- TiroAoAlvo/jogo.js
+++ TiroAoAlvo/jogo.js
@@ -1,4 +1,3 @@
-
 var largura = 0;
 var altura = 0;
 
@@ -6,25 +5,50 @@ function ajustaTamanhoPalcoJogo() {
 	largura = window.innerWidth;
 	altura = window.innerHeight;
 
-	console.log(largura, altura);
 }
 
 ajustaTamanhoPalcoJogo();
 
 function posicaoRandomica() {
-	var posicaoX = Math.floor(Math.random() * largura) - 50;
-	posicaoX = posicaoX < 0 ? 0 : posicaoX;
-
-	var posicaoY = Math.floor(Math.random() * altura) - 50;
-	posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
 	var alvo = document.createElement("img");
 	alvo.src = "imagens/alvo.png";
 	alvo.className = "alvo1";
+
+	document.body.appendChild(alvo);
+
+	//impede de que a imagem ultrapasse os limites de largura da tela
+	var posicaoX = Math.floor(Math.random() * largura) - larguraCSS(alvo);
+	posicaoX = posicaoX < 0 ? 0 : posicaoX;
+
+	//impede de que a imagem ultrapasse os limites de altura da tela
+	var posicaoY = Math.floor(Math.random() * altura) - alturaCSS(alvo);
+	posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
 	alvo.style.left = posicaoX + "px";
 	alvo.style.top = posicaoY + "px";
 	alvo.style.position = "absolute";
 
-	document.body.appendChild(alvo);
 }
 
+
+//retorna a largura do elemento no CSS
+function larguraCSS(elemento) {
+	var largura = document.defaultView.getComputedStyle(elemento).getPropertyValue("width");
+	//retorna o valor em string: "VALORpx"
+	var tamanho = largura.length;
+	//remove o "px" da string
+	largura = largura.substring(0, tamanho - 2);
+
+	return parseInt(largura);
+}
+
+//retorna a altura do elemento no CSS
+function alturaCSS(elemento) {
+	var altura = document.defaultView.getComputedStyle(elemento).getPropertyValue("height");
+	//retorna o valor em string: "VALORpx"
+	var tamanho = altura.length;
+	//remove o "px" da string
+	altura = altura.substring(0, tamanho - 2);
+	return parseInt(altura);
+}
