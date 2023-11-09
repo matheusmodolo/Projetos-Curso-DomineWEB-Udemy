3dc17b16c3e91bbd59b46d6bd29fab57544a72a2
Matheus Módolo <nmmatheus@hotmail.com>
2023-11-09 16:57:24 -0300

    validar dados antes de registrá-los

--- OrçamentoPessoal/app.js
+++ OrçamentoPessoal/app.js
@@ -7,24 +7,36 @@ class Despesa {
     this.descricao = descricao;
     this.valor = valor;
   }
+
+  validarDados() {
+    for (let i in this) {
+      if (this[i] == undefined || this[i] == "" || this[i] == null) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 class Bd {
   constructor() {
-    let id = localStorage.getItem();
+    let id = localStorage.getItem("id");
+
     if (id === null) {
       localStorage.setItem("id", 0);
     }
   }
 
   getProximoId() {
-    let proximoId = localStorage.getItem();
+    let proximoId = localStorage.getItem("id");
     return parseInt(proximoId) + 1;
   }
 
   gravar(d) {
     let id = this.getProximoId();
+
     localStorage.setItem(id, JSON.stringify(d));
+
     localStorage.setItem("id", id);
   }
 }
@@ -32,7 +44,6 @@ class Bd {
 let bd = new Bd();
 
 function cadastrarDespesa() {
-  //recuperar os valores dos campos
   let ano = document.getElementById("ano");
   let mes = document.getElementById("mes");
   let dia = document.getElementById("dia");
@@ -40,7 +51,6 @@ function cadastrarDespesa() {
   let descricao = document.getElementById("descricao");
   let valor = document.getElementById("valor");
 
-  //criação do objeto
   let despesa = new Despesa(
     ano.value,
     mes.value,
@@ -50,5 +60,36 @@ function cadastrarDespesa() {
     valor.value
   );
 
-  bd.gravar(despesa);
+  if (despesa.validarDados()) {
+    bd.gravar(despesa);
+    //dialog de sucesso
+    document.getElementById("modal_titulo_cor").className =
+      "modal-header text-success";
+
+    document.getElementById("modal_titulo").innerHTML =
+      "Registro inserido com sucesso";
+
+    document.getElementById("modal_conteudo").innerHTML =
+      "Despesa cadastrada com sucesso!";
+
+    document.getElementById("modal_btn").className = "btn btn-success";
+    document.getElementById("modal_btn").innerHTML = "Voltar";
+
+    $("#modal_registrar").modal("show");
+  } else {
+    //dialog de erro
+    document.getElementById("modal_titulo_cor").className =
+      "modal-header text-danger";
+
+    document.getElementById("modal_titulo").innerHTML =
+      "Erro na inclusão do registro";
+
+    document.getElementById("modal_conteudo").innerHTML =
+      "Erro na gravação, verifique se todos os campos foram preenchidos corretamente.";
+
+    document.getElementById("modal_btn").className = "btn btn-danger";
+    document.getElementById("modal_btn").innerHTML = "Voltar e corrigir";
+
+    $("#modal_registrar").modal("show");
+  }
 }
