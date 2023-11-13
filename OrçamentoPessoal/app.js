8eb96ecf5b44ffe72c2a4e4cb92947b3915714f7
Matheus Módolo <nmmatheus@hotmail.com>
2023-11-13 12:02:01 -0300

    listando despesas

--- OrçamentoPessoal/app.js
+++ OrçamentoPessoal/app.js
@@ -39,6 +39,20 @@ class Bd {
 
     localStorage.setItem("id", id);
   }
+  recuperarTodosRegistros() {
+    let despesas = Array();
+    let id = localStorage.getItem("id");
+
+    for (let i = 1; i <= id; i++) {
+      let despesa = JSON.parse(localStorage.getItem(i));
+
+      if (despesa === null) {
+        continue;
+      }
+      despesas.push(despesa);
+    }
+    return despesas;
+  }
 }
 
 let bd = new Bd();
@@ -92,4 +106,41 @@ function cadastrarDespesa() {
 
     $("#modal_registrar").modal("show");
   }
+}
+
+function carregaListaDespesas() {
+  let despesas = Array();
+  despesas = bd.recuperarTodosRegistros();
+
+  let listaDespesas = document.getElementById("listaDespesas");
+
+  despesas.forEach(function (d) {
+    //criando linha
+    let linha = listaDespesas.insertRow();
+
+    //criando colunas
+    linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+    
+    switch (d.tipo) {
+      case "1":
+        d.tipo = "Alimentação";
+        break;
+      case "2":
+        d.tipo = "Educação";
+        break;
+      case "3":
+        d.tipo = "Lazer";
+        break;
+      case "4":
+        d.tipo = "Saúde";
+        break;
+      case "4":
+        d.tipo = "Transporte";
+        break;
+    }
+
+    linha.insertCell(1).innerHTML = d.tipo;
+    linha.insertCell(2).innerHTML = d.descricao;
+    linha.insertCell(3).innerHTML = d.valor;
+  });
 }
