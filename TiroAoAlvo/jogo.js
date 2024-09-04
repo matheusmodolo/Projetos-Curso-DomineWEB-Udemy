a3dcc325d416bb781a0b3b08a198ea6b7e7e6059
Matheus <nmmatheus@hotmail.com>
2024-09-03 22:00:09 -0300

    "Updated HTML, CSS, and JavaScript files for Tiro Ao Alvo game, including changes to layout, styling, and game logic."

--- TiroAoAlvo/jogo.js
+++ TiroAoAlvo/jogo.js
@@ -9,111 +9,115 @@ nivel = nivel.replace("?", "");
 console.log(nivel);
 
 switch (nivel) {
-	case "facil":
-		criaAlvoTempo = 1500;
-		break;
-	case "medio":
-		criaAlvoTempo = 1000;
-		break;
-	case "dificil":
-		criaAlvoTempo = 750;
-		break;
-	default:
-		break;
+  case "facil":
+    criaAlvoTempo = 1500;
+    break;
+  case "medio":
+    criaAlvoTempo = 1000;
+    break;
+  case "dificil":
+    criaAlvoTempo = 750;
+    break;
+  case "expert":
+    criaAlvoTempo = 500;
+    break;
+  default:
+    break;
 }
 
 var cronometro = setInterval(function () {
+  if (tempo < 0) {
+    clearInterval(cronometro);
+    clearInterval(criaAlvo);
 
-	if (tempo < 0) {
-		clearInterval(cronometro);
-		clearInterval(criaAlvo);
+    window.location.href = "vitoria.html";
+  } else {
+    document.getElementById("cronometro").innerHTML = tempo;
+  }
 
-		window.location.href = "vitoria.html";
-
-	} else {
-		document.getElementById("cronometro").innerHTML = tempo;
-	}
-
-	tempo--;
-
+  tempo--;
 }, 1000);
 
 function ajustaTamanhoPalcoJogo() {
-	largura = window.innerWidth;
-	altura = window.innerHeight;
-
+  largura = window.innerWidth;
+  altura = window.innerHeight;
 }
 
 ajustaTamanhoPalcoJogo();
 
 function posicaoRandomica() {
+  //remover o alvo anterior (caso exista)
+  if (document.getElementById("alvo")) {
+    document.getElementById("alvo").remove();
 
-	//remover o alvo anterior (caso exista)
-	if (document.getElementById("alvo")) {
-		document.getElementById("alvo").remove();
+    if (vida > 3) {
+      window.location.href = "fim_de_jogo.html";
+    }
+    document.getElementById("v" + vida).src = "imagens/coracao_vazio.png";
 
-		if (vida > 3) {
-			window.location.href = "fim_de_jogo.html";
-		}
-		document.getElementById("v" + vida).src = "imagens/coracao_vazio.png";
+    vida++;
+  }
 
-		vida++;
-	}
+  var alvo = document.createElement("img");
+  alvo.src = "imagens/alvo.png";
+  alvo.className = tamanhoAleatorio();
 
-	var alvo = document.createElement("img");
-	alvo.src = "imagens/alvo.png";
-	alvo.className = tamanhoAleatorio();
+  document.body.appendChild(alvo);
 
-	document.body.appendChild(alvo);
+  //impede de que a imagem ultrapasse os limites de largura da tela
+  var posicaoX = Math.floor(Math.random() * largura) - larguraCSS(alvo);
+  posicaoX = posicaoX < 0 ? 0 : posicaoX;
 
-	//impede de que a imagem ultrapasse os limites de largura da tela
-	var posicaoX = Math.floor(Math.random() * largura) - larguraCSS(alvo);
-	posicaoX = posicaoX < 0 ? 0 : posicaoX;
+  //impede de que a imagem ultrapasse os limites de altura da tela
+  var posicaoY = Math.floor(Math.random() * altura) - alturaCSS(alvo);
+  posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
-	//impede de que a imagem ultrapasse os limites de altura da tela
-	var posicaoY = Math.floor(Math.random() * altura) - alturaCSS(alvo);
-	posicaoY = posicaoY < 0 ? 0 : posicaoY;
+  alvo.style.transition = "opacity " + criaAlvoTempo * 1.5 + "ms";
+  alvo.style.opacity = 0;
+  alvo.style.left = posicaoX + "px";
+  alvo.style.top = posicaoY + "px";
+  alvo.style.position = "absolute";
+  alvo.id = "alvo";
 
-	alvo.style.left = posicaoX + "px";
-	alvo.style.top = posicaoY + "px";
-	alvo.style.position = "absolute";
-	alvo.id = "alvo";
-
-	alvo.onclick = function () {
-		this.remove();
-	};
+  alvo.onclick = function () {
+    this.remove();
+  };
 }
 
 //retorna a largura do elemento no CSS
 function larguraCSS(elemento) {
-	var largura = document.defaultView.getComputedStyle(elemento).getPropertyValue("width");
-	//retorna o valor em string: "VALORpx"
-	var tamanho = largura.length;
-	//remove o "px" da string
-	largura = largura.substring(0, tamanho - 2);
+  var largura = document.defaultView
+    .getComputedStyle(elemento)
+    .getPropertyValue("width");
+  //retorna o valor em string: "VALORpx"
+  var tamanho = largura.length;
+  //remove o "px" da string
+  largura = largura.substring(0, tamanho - 2);
 
-	return parseInt(largura);
+  return parseInt(largura);
 }
 
 //retorna a altura do elemento no CSS
 function alturaCSS(elemento) {
-	var altura = document.defaultView.getComputedStyle(elemento).getPropertyValue("height");
-	//retorna o valor em string: "VALORpx"
-	var tamanho = altura.length;
-	//remove o "px" da string
-	altura = altura.substring(0, tamanho - 2);
-	return parseInt(altura);
+  var altura = document.defaultView
+    .getComputedStyle(elemento)
+    .getPropertyValue("height");
+  //retorna o valor em string: "VALORpx"
+  var tamanho = altura.length;
+  //remove o "px" da string
+  altura = altura.substring(0, tamanho - 2);
+  return parseInt(altura);
 }
 
 function tamanhoAleatorio() {
-	var classe = Math.floor(Math.random() * 3);
+  var classe = Math.floor(Math.random() * 3);
 
-	switch (classe) {
-		case 0:
-			return "alvo1";
-		case 1:
-			return "alvo2";
-		case 2:
-			return "alvo3";
-	}
+  switch (classe) {
+    case 0:
+      return "alvo1";
+    case 1:
+      return "alvo2";
+    case 2:
+      return "alvo3";
+  }
 }
